cfa3e99d36f101fbae2a0d010ddb6c7851c97e89
yojo5354-web <9361820@qq.com>
2026-03-24 19:52:00 +0800

    Upload next.config.js

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: '**' },
+      { protocol: 'http', hostname: 'localhost' },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:3001/api/:path*'
+      }
+    ];
+  }
+}
+
+module.exports = nextConfig
