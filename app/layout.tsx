247c73d1879c32cd3e33c682d5acc36b94b939b5
yojo5354-web <9361820@qq.com>
2026-03-24 20:38:51 +0800

    Fix build error: code/web/app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <body>
         {children}
         <Toaster 
-          position="top center"
+          position="top-center"
           toastOptions={{
             duration: 2000,
             style: {
